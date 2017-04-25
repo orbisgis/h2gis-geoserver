5ee31d25ee45c99945de2345808e808c9e8d1339
ebocher <erwan.bocher@univ-ubs.fr>
2017-04-25 21:33:59 +0200

    Improve H2GIS Geoserver Link it with the H2GIS-Geotools driver Add basic unit test (same as in Geoserver)

--- src/main/java/org/h2gis/geoserver/H2GISDataStoreFactoryInitializer.java
+++ src/main/java/org/h2gis/geoserver/H2GISDataStoreFactoryInitializer.java
@@ -1,33 +1,33 @@
 /*
- * h2gis-gs is an extension to geoserver to connect H2GIS a spatial library 
+ * h2gis-geoserver is an extension to geoserver to connect H2GIS a spatial library 
  * that brings spatial support to the H2 database engine.
  *
- * h2gis-gs  is distributed under GPL 3 license. It is produced by the DECIDE
+ * h2gis-geoserver  is distributed under GPL 3 license. It is produced by the DECIDE
  * team of the Lab-STICC laboratory <http://www.labsticc.fr/> CNRS UMR 6285.
  *
- * Copyright (C) 2015-2016 Lab-STICC (CNRS UMR 6285)
+ * Copyright (C) 2015-2017 Lab-STICC (CNRS UMR 6285)
  *
- * h2gis-gs  is free software: you can redistribute it and/or modify it under the
+ * h2gis-geoserver  is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
  *
- * h2gis-gs  is distributed in the hope that it will be useful, but WITHOUT ANY
+ * h2gis-geoserver  is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * h2gis-gs. If not, see <http://www.gnu.org/licenses/>.
+ * h2gis-geoserver. If not, see <http://www.gnu.org/licenses/>.
  *
- * For more information, please consult: <http://www.orbisgis.org/>
+ * For more information, please consult: <http://www.h2gis.org/>
  * or contact directly:
- * info_at_ orbisgis.org
+ * info_at_ h2gis.org
  */
-package org.orbisgis.geoserver.h2gis;
+package org.h2gis.geoserver;
 
 import org.geoserver.data.DataStoreFactoryInitializer;
-import org.orbisgis.geoserver.h2gis.datastore.H2GISDataStoreFactory;
 import org.geoserver.platform.GeoServerResourceLoader;
+import org.h2gis.geotools.H2GISDataStoreFactory;
 
 /**
  * Initializes an H2GIS data store factory setting its location to the geoserver
